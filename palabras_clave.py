7a50adf653443422c6800c3663973348b50ec73e
YSantanas <44857937+YSantanas@users.noreply.github.com>
2023-11-25 23:50:21 -0600

    backend completo

---
+++ palabras_clave.py
@@ -0,0 +1,114 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov 24 22:40:26 2023
+
+@author: Santana Sánchez María Yvette
+"""
+
+# Palabras clave para cada categoría
+palabras_clave_definicion = [
+        "es",
+        "constituye",
+        "consiste",
+        "se",
+        "define",
+        "denomina",
+        "conoce",
+        "entiende",
+        "trata",
+        "refiere",
+        "llama",
+    ]
+palabras_clave_componentes = [
+        "contiene",
+        "compone",
+        "comprende",
+        "incluye",
+        "tiene",
+        "encuentra",
+        "ubica",
+        "componentes",
+        "partes",
+        "elementos",
+        "divide",
+        "separa",
+        "clasifica",
+        "organiza",
+        "agrupa",
+        "estructura",
+    ]
+
+palabras_clave_funcionalidades = [
+        "realiza",
+        "hace",
+        "sirve",
+        "utiliza",
+        "usado",
+        "usada",
+        "utilizado",
+        "utilizada",
+        "usa",
+        "utiliza",
+        "emplea",
+        "sirve",
+        "funciona",
+        "funcionan",
+        "funcionando",
+        "funciones",
+        "funcionalidades",
+    ]
+
+
+###################################
+##########  I N G L E S   #######
+###################################
+
+
+palabras_clave_definicion2 = [
+    "is",
+    "constitutes",
+    "consists",
+    "defines",
+    "denotes",
+    "knows",
+    "understands",
+    "deals with",
+    "refers to",
+    "calls",
+]
+
+palabras_clave_componentes2 = [
+    "contains",
+    "comprises",
+    "includes",
+    "has",
+    "is found",
+    "locates",
+    "places",
+    "components",
+    "parts",
+    "elements",
+    "divides",
+    "separates",
+    "classifies",
+    "organizes",
+    "groups",
+    "structures",
+]
+
+palabras_clave_funcionalidades2 = [
+    "performs",
+    "does",
+    "serves",
+    "uses",
+    "used",
+    "uses",
+    "uses",
+    "employs",
+    "serves",
+    "works",
+    "function",
+    "functioning",
+    "functions",
+    "functionalities",
+]
